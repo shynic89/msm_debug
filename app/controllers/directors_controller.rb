3de6832bd71fe52dc3b1572ad17886178b5554d9
Nick Su <hsu0@chicagobooth.edu>
2017-07-25 23:13:21 +0000

    test 1

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,21 +1,22 @@
 class DirectorsController < ApplicationController
   def index
-    directors = Director.all
+    @directors = Director.all
   end
 
   def show
     @director = Director.find(params[:id])
   end
 
-  def new
+  def new_form
+    @director = Director.new
   end
 
   def create_row
     @director = Director.new
-    @director.dob = params[:dob]
-    @director.name = params[:name]
-    @director.bio = params[:bio]
-    @director.image_url = params[:image_url]
+    @director.dob = params["dob"]
+    @director.name = params["name"]
+    @director.bio = params["bio"]
+    @director.image_url = params["image_url"]
 
     @director.save
 
@@ -29,11 +30,10 @@ class DirectorsController < ApplicationController
   def update_row
     @director = Director.find(params[:id])
 
-    @director.dob = params[":dob"]
-    @director.name = params[":name"]
-    @director.bio = params[":bio"]
-    @director.image_url = params[":image_url"]
-
+    @director.dob = params["the_dob"]
+    @director.name = params["the_name"]
+    @director.bio = params["the_bio"]
+    @director.image_url = params["the_image_url"]
     @director.save
 
     render("show")
